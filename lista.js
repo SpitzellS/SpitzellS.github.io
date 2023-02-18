913767d56aee528284f74571254dc337eec4c055
SpitzellS <108983048+SpitzellS@users.noreply.github.com>
2023-02-18 10:26:00 +0100

    Add files via upload

--- lista.js
+++ lista.js
@@ -1,18 +1,16 @@
 function deletePerm() {
-
     if (!localStorage.getItem('playlistSp')) {
         localStorage.setItem('playlistSp', '{}')
 
         if(variosAnos) {
-
             const webm = lista2[posicion-1].link
-        
             const playlistSp = JSON.parse(localStorage.getItem('playlistSp'))
             
             localStorage.setItem('playlistSp', JSON.stringify({
                 ...playlistSp,
             [webm]: {
-                    eliminada: true
+                    eliminada: true,
+                    name: lista2[posicion-1].name
                 }
             }))
         } else {
@@ -22,21 +20,22 @@ function deletePerm() {
             localStorage.setItem('playlistSp', JSON.stringify({
                 ...playlistSp,
             [webm]: {
-                    eliminada: true
+                    eliminada: true,
+                    name: lista[posicion-1].name
                 }
             }))
         }
     } else {
         
         if(variosAnos) {
-            const webm = lista2[posicion-1].link
-        
+            const webm = lista2[posicion-1].link   
             const playlistSp = JSON.parse(localStorage.getItem('playlistSp'))
     
             localStorage.setItem('playlistSp', JSON.stringify({
                 ...playlistSp,
             [webm]: {
-                    eliminada: true
+                    eliminada: true,
+                    name: lista2[posicion-1].name
                 }
             }))
         } else {
@@ -46,7 +45,8 @@ function deletePerm() {
             localStorage.setItem('playlistSp', JSON.stringify({
                 ...playlistSp,
             [webm]: {
-                    eliminada: true
+                    eliminada: true,
+                    name: lista[posicion-1].name
                 }
             }))
         }
