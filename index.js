70d7d70b70e381ceef0a59d278531808b7ab613b
SpitzellS <108983048+SpitzellS@users.noreply.github.com>
2025-07-06 12:02:30 +0200

    Update index.js

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ document.addEventListener("DOMContentLoaded", function() {
             seconds: 25,
             difficultyMin: 0,
             difficultyMax: 100,
-            anoMin: 2020,
+            anoMin: 2017,
             anoMax: 2024,
             user: "",
             op: true,
